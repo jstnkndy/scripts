cebcef8105ce918a00cb5be9d5b48455f39a33af
Justin Kennedy <jstnkndy@gmail.com>
2015-10-20 08:41:39 -0400

    Revert "added parse_ntds.py"
    This reverts commit a4cc4a51876a7d37b7d684ab369f684ea6233a11.

--- parse_wdigest.py
+++ parse_wdigest.py
@@ -8,12 +8,10 @@ Author:		Justin Kennedy (@jstnkndy)
 -------------------------------------------------------------------------------
 """
 
-import sys, re
-
+import sys,re
 
 def usage():
     print 'Usage: python {} <input file>'.format(sys.argv[0])
-
 
 def main():
     if len(sys.argv) != 2:
@@ -23,19 +21,18 @@ def main():
     username_regex = re.compile(r'\*\s+Username\s+:\s+(.*)')
     domain_regex = re.compile(r'\*\sDomain\s+:\s(.*)')
     password_regex = re.compile(r'\*\s+Password\s+:\s+(.*)')
-
+	
     usernames, domains, passwords = [], [], []
-
+	
     with open(sys.argv[1], 'r') as input:
         data = input.read()
 
         [usernames.append(username) for username in re.findall(username_regex, data)]
         [domains.append(domain) for domain in re.findall(domain_regex, data)]
         [passwords.append(password) for password in re.findall(password_regex, data)]
-
+			
     for i in range(0, len(usernames)):
         print '{}\\{} - {}'.format(domains[i], usernames[i], passwords[i])
-
 
 if __name__ == '__main__':
     main()
