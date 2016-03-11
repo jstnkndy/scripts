b113ebd11af7606aa13bc4bb5c6db5e60509921d
Justin Kennedy <jstnkndy@gmail.com>
2016-03-10 23:34:25 -0500

    appeasing drone

--- dns_listener.py
+++ dns_listener.py
@@ -9,25 +9,37 @@ Author:		Justin Kennedy (@jstnkndy)
 """
 
 import sys
+import argparse
+import signal
 from scapy.all import *
 
-def usage():
-    print 'Usage: {} <domain>'.format(sys.argv[0])
 
 def pkt_callback(pkt, domain):
     if DNSQR in pkt:
         if domain in pkt[DNS].qd.qname:
             print pkt[DNS].qd.qname
 
+def signal_handler(signal, frame):
+    print "Caught signal, exiting!";
+    sys.exit(signal)
 
 def main():
-    if len(sys.argv) != 1:
-        usage()
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--interface", help="Interface to sniff on")
+    parser.add_argument("--domain", help="String to match on")
+
+    if len(sys.argv) != 5:
+        parser.print_help()
         sys.exit(1)
 
-    interface = 'eth0'
+    signal.signal(signal.SIGINT, signal_handler)
+
+    args = parser.parse_args()
+
+    interface = args.interface
+    domain = args.domain
     filter = 'udp and port 53'
-    domain = sys.argv[1]
 
     sniff(iface=interface, filter=filter, prn=lambda x: pkt_callback(x, domain))
 
